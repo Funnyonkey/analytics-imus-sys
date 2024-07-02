83b4c489333b9a2c3840e760b73f25da369b3217
Funnyonkey <renzmark.abut@cvsu.edu.ph>
2024-07-02 12:37:12 +0800

    Change config

--- config.js
+++ config.js
@@ -1,16 +1,16 @@
 
 // const config = {
-//     host: 'pesodatabase.cpeies8qgyq4.ap-southeast-1.rds.amazonaws.com',
-//     user: 'admin',
-//     password: 'testing123',
+//     host: 'localhost',
+//     user: 'root',
+//     password: '',
 //     database: 'peso_db'
 // };
 
 const config = {
-    host: 'sql.freedb.tech',
-    user: 'freedb_peso_admin',
-    password: 'qJnJ!4U!qPe7Aw5',
-    database: 'freedb_peso_db'
+    host: 'peso-database.cpeies8qgyq4.ap-southeast-1.rds.amazonaws.com',
+    user: 'admin',
+    password: 'testing123',
+    database: 'peso_db'
 };
 
 module.exports = config; 
